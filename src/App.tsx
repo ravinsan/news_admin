36f4228ad7737f951bf3058b2d56641c9aaa8675
ravinsan <39220869+ravinsan@users.noreply.github.com>
2025-03-01 06:30:42 +0530

    add dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { Button } from "./components/ui/button"
+import Page from "./app/dashboard/page"
 
 function App() {
   return (
     <>
-       <h1 className="text-3xl font-bold underline">Hello World!</h1>
-       <Button variant={"destructive"}>New Button</Button>
+       <Page />
     </>
   )
 }
